dc84389b1a973241f514e20f5c33f1ab059f3a5c
이소영 <th42500@naver.com>
2023-11-13 11:15:31 +0900

    feat: Generic이 아닌 any를 사용한다면?

--- learn-typescript/practice/10_Generic.ts
+++ learn-typescript/practice/10_Generic.ts
@@ -1,7 +1,13 @@
-function getText(text: string): string {
+// 1. 타입을 미리 지정하지 않고 호출할 때 타입을 지정하는 이유
+/* function getText(text: string): string {
   return text;
 }
 
 function getNumber(text: number): number {
+  return text;
+} */
+
+// 2. any를 사용하면 되지 않을까?
+function getText(text: any): any {
   return text;
 }
