aade2219654d3f5fdc8fb77b3e336c1b0ca17f91
이소영 <th42500@naver.com>
2023-11-14 10:46:48 +0900

    feat: 더 복잡한 경우의 타입 가드 실습

--- learn-typescript/practice/14_TypeGuardFunc.ts
+++ learn-typescript/practice/14_TypeGuardFunc.ts
@@ -6,17 +6,26 @@ interface Person {
 interface Developer {
   name: string;
   skill: string;
+  age: string;
 }
 
-function isPerson(someone: Person | Developer): someone is Person {
-  return (someone as Person).age !== undefined;
+interface Hero {
+  name: string;
+  nickname: string;
+}
+
+function isPerson(someone: Person | Developer | Hero): someone is Person {
+  return typeof (someone as Person).age === 'number';
 } 
 
-function greet(someone: Person | Developer) {
+function isDeveloper(someone: Person | Developer | Hero): someone is Developer {
+  return typeof (someone as Developer).age === 'string';
+}
+
+function greet(someone: Person | Developer | Hero) {
   if(isPerson(someone)) {
     console.log("사람의 나이는 : " + someone.age);
-  } else {
+  } else if(isDeveloper(someone)){
     console.log("개발자의 역량은 : " + someone.skill);
   }
 }
-
